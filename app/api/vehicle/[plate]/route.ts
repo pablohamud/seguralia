93bb8c8e533c8970f3367ca7bd32ed1b78d97541
Pablo Hamud <pablohamud00@gmial.com>
2026-06-02 12:15:02 -0300

    debug clasific

--- app/api/vehicle/[plate]/route.ts
+++ app/api/vehicle/[plate]/route.ts
@@ -7,34 +7,38 @@ export async function GET(
   const { plate } = await params;
   const normalizedPlate = plate.toUpperCase();
 
-  console.log("API KEY:", process.env.CLASIFIC_API_KEY ? "existe" : "VACÍA");
-try {
-  const response = await fetch(
-    `https://api.clasific.ar/v1/vehicles/basic?plate=${normalizedPlate}&classification=true`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.CLASIFIC_API_KEY}`,
-      },
-    }
+  console.log(
+    "API KEY:",
+    process.env.CLASIFIC_API_KEY ? "existe" : "VACÍA"
   );
 
-  console.log("STATUS:", response.status);
+  try {
+    const response = await fetch(
+      `https://api.clasific.ar/v1/vehicles/basic?plate=${normalizedPlate}&classification=true`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.CLASIFIC_API_KEY}`,
+        },
+      }
+    );
 
-  const text = await response.text();
+    console.log("STATUS:", response.status);
 
-  console.log("BODY:", text);
+    const text = await response.text();
 
-  // TEMPORAL PARA DEBUG
-  return NextResponse.json({
-    status: response.status,
-    body: text,
-  });
+    console.log("BODY:", text);
 
-} catch (error) {
-  console.error("Error:", error);
+    // TEMPORAL PARA DEBUG
+    return NextResponse.json({
+      status: response.status,
+      body: text,
+    });
+  } catch (error) {
+    console.error("Error:", error);
 
-  return NextResponse.json(
-    { error: "Error al consultar la API" },
-    { status: 500 }
-  );
+    return NextResponse.json(
+      { error: "Error al consultar la API" },
+      { status: 500 }
+    );
+  }
 }
